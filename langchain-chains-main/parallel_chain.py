47cf0c1b19ea8e0e01cf664caa0565c127eaf962
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-07-15 15:12:29 +0500

    new

--- langchain-chains-main/parallel_chain.py
+++ langchain-chains-main/parallel_chain.py
@@ -41,7 +41,6 @@ text = """
 Support vector machines (SVMs) are a set of supervised learning methods used for classification, regression and outliers detection.
 
 The advantages of support vector machines are:
-
 Effective in high dimensional spaces.
 
 Still effective in cases where number of dimensions is greater than the number of samples.
