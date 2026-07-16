af70e738d4ab82f6a3b83b2fba5db784ccdab53b
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-07-16 10:26:04 +0500

    new

--- Langchain Models/3.EmbeddedModels/Embeding_hf_local.py
+++ Langchain Models/3.EmbeddedModels/Embeding_hf_local.py
@@ -3,7 +3,16 @@ from langchain_huggingface import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
-text = "what is capital of Pakistan?"
+# text = "what is capital of Pakistan?"
 
-vector = embeddings.embed_query(text)
-print(str(vector))
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+vectors = embeddings.embed_documents(documents)
+
+print("Document Vectors:" + str(vectors))
