1a8778ba653c99a55412cfb38607a1fcf2a6501c
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-07-16 10:40:41 +0500

    new

--- Langchain Models/3.EmbeddedModels/4_document_similarity.py
+++ Langchain Models/3.EmbeddedModels/4_document_similarity.py
@@ -1,4 +1,4 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -6,14 +6,11 @@ import os
 
 load_dotenv()
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-if not openai_api_key or not openai_api_key.startswith("sk-") or openai_api_key.startswith("sk-proj-"):
-    raise ValueError("Please set a valid OPENAI_API_KEY environment variable. The key should start with 'sk-' but not 'sk-proj-'")
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# if not openai_api_key or not openai_api_key.startswith("sk-") or openai_api_key.startswith("sk-proj-"):
+#     raise ValueError("Please set a valid OPENAI_API_KEY environment variable. The key should start with 'sk-' but not 'sk-proj-'")
 
-embedding = OpenAIEmbeddings(
-    model='text-embedding-3-large',
-    model_kwargs={"dimensions": 300}
-)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 documents = [
     "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
@@ -25,13 +22,19 @@ documents = [
 
 query = 'tell me about bumrah'
 
-doc_embeddings = embedding.embed_documents(documents)
-query_embedding = embedding.embed_query(query)
+doc_embeddings = embeddings.embed_documents(documents)
+query_embedding = embeddings.embed_query(query)
 
 scores = cosine_similarity([query_embedding], doc_embeddings)[0]
 
-index = np.argmax(scores)
-score = scores[index]
+# index = np.argmax(scores)
+# score = scores[index]
+
+# print(f"Query: {query}")
+# print(f"Most similar document: {documents[index]}")
+# print(f"Similarity score: {score}")
+
+index, score = sorted(list(enumerate(scores)), key=lambda x: x[1])[-1] 
 
 print(f"Query: {query}")
 print(f"Most similar document: {documents[index]}")
