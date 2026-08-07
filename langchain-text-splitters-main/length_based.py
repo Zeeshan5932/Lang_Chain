4542a2242017c86cdd7e622d6f9c7220422d2a14
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-08-07 15:29:34 +0500

    new

--- langchain-text-splitters-main/length_based.py
+++ langchain-text-splitters-main/length_based.py
@@ -1,9 +1,22 @@
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader('dl-curriculum.pdf')
+# loader = PyPDFLoader('dl-curriculum.pdf')
 
-docs = loader.load()
+# docs = loader.load()
+
+text = """
+        2. 💰 "Aloo Ko 1 Crore Mil Gaye"
+
+Hook:
+Bank Manager:
+"Congratulations! Aapke account me 1 crore transfer hue hain."
+
+Curiosity:
+Sab sochte hain kisne bheje?
+
+Twist:
+Galti se kisi ne "Aloo Mattar Gobi Restaurant" ki payment iske account me bhej di."""
 
 splitter = CharacterTextSplitter(
     chunk_size=200,
@@ -11,6 +24,6 @@ splitter = CharacterTextSplitter(
     separator=''
 )
 
-result = splitter.split_documents(docs)
-
-print(result[3].page_content)
+result = splitter.split_documents(text)
+print(result)
+# print(result[3].page_content)
